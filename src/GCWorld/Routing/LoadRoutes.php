ec0cfa51015c8b23e4a5768b8c91e7c45a01158a
Mitch <mitch@viethconsulting.com>
2014-12-22 23:53:45 -0500

    Debug

--- src/GCWorld/Routing/LoadRoutes.php
+++ src/GCWorld/Routing/LoadRoutes.php
@@ -46,7 +46,7 @@ class LoadRoutes
 		}
 
 		$base = dirname(__FILE__).'/Generated/';
-		$files = scandir($base);
+		$files = self::glob_recursive($base);
 		foreach($files as $file)
 		{
 			d($file);
@@ -78,5 +78,15 @@ class LoadRoutes
 			$processor = new Processor();
 			$processor->run($routes);
 		}
+	}
+
+	private static function glob_recursive($pattern, $flags = 0)
+	{
+		$files = glob($pattern, $flags);
+		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+		{
+			$files = array_merge($files, self::glob_recursive($dir.'/'.basename($pattern), $flags));
+		}
+		return $files;
 	}
 }
